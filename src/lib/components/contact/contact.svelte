82314f7356a1772c53fb40a3bdd2837e34465e29
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-28 22:27:42 -0700

    edit contact

--- src/lib/components/contact/contact.svelte
+++ src/lib/components/contact/contact.svelte
@@ -3,7 +3,8 @@
 
 <main>
 	<div class="text">
-		<h2>Holla.</h2>
+		<h2>Contact us by Email at: <a href="mailto: hi@aufbau.io">hi@aufbau.io</a></h2>
+		<h2>Drop us a line, don't be shy xx</h2>
 	</div>
 </main>
 
@@ -12,22 +13,31 @@
 		position: relative;
 		min-height: 100vh;
 		max-height: 100vh;
+		inline-size: calc(100%);
+		background: #d0d0d0;
 	}
 
 	.text {
 		position: absolute;
 		top: 100px;
 		left: 5%;
-		display: flex;
-		justify-content: space-between;
 		user-select: none;
 		color: var(--white);
+		inline-size: calc(100% - 20px);
 	}
 
 	.text h2 {
 		font-size: 150px;
 		line-height: 150px;
 		user-select: none;
-		font-family: nb-television-2d, sans-serif;
+		font-family: nb-architekt, sans-serif;
+		overflow-wrap: break-word;
+		inline-size: calc(100% - 20px);
+		line-height: 105%;
+		opacity: 0.9;
+	}
+
+	a {
+		color: var(--black);
 	}
 </style>
