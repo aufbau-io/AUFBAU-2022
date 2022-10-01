51db129c197fef7b38b48153f9668bed81f4f539
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-10-01 16:05:35 -0700

    edit

--- src/lib/components/pages/about.svelte
+++ src/lib/components/pages/about.svelte
@@ -20,49 +20,6 @@
 			you're down to make some magic, get in touch.
 		</h6>
 	</div>
-	<!--span class="muted">
-		<h2>THAT AUFBAU</h2>
-		<h6 class="text">
-			From 1922 to 1925, Rudolf Carnap worked on a book which became one of his major works, namely
-			Der logische Aufbau der Welt (translated as The Logical Structure of the World, 1967), which
-			was accepted in 1926 as his habilitation thesis at the University of Vienna and published as a
-			book in 1928. That achievement has become a landmark in modern epistemology and can be read as
-			a forceful statement of the philosophical thesis of logical positivism. Indeed, the Aufbau
-			suggests that epistemology, based on modern symbolic logic, is concerned with the logical
-			analysis of scientific propositions, while science itself, based on experience, is the only
-			source of knowledge of the external world, i.e. the world outside the realm of human
-			perception. According to Carnap, philosophical propositions are statements about the language
-			of science; they aren't true or false, but merely consist of definitions and conventions about
-			the use of certain concepts. In contrast, scientific propositions are factual statements about
-			the external reality. They are meaningful because they are based on the perceptions of the
-			senses. In other words, the truth or falsity of those propositions can be verified by testing
-			their content with further observations. In the Aufbau, Carnap wants to display the logical
-			and conceptual structure with which all scientific (factual) statements can be organized.
-			Carnap gives the label "constitution theory" to this epistemic-logical project. It is a
-			constructive undertaking that systematizes scientific knowledge according to the notions of
-			symbolic logic. Accordingly, the purpose of this constitutional system is to identify and
-			discern different classes of scientific concepts and to specify the logical relations that
-			link them. In the Aufbau, concepts are taken to denote objects, relations, properties, classes
-			and states. Carnap argues that all concepts must be ranked over a hierarchy. In that
-			hierarchy, all concepts are organized according to a fundamental arrangement where concepts
-			can be reduced and converted to other basic ones. Carnap explains that a concept can be
-			reduced to another when all sentences containing the first concept can be transformed into
-			sentences containing the other. In other words, every scientific sentence should be
-			translatable into another sentence such that the original terms have the same reference as the
-			translated terms. Most significantly, Carnap argues that the basis of this system is
-			psychological. Its content is the "immediately given", which is made of basic elements, namely
-			perceptual experiences. These basic elements consist of conscious psychological states of a
-			single human subject. In the end, Carnap argues that his constitutional project demonstrates
-			the possibility of defining and uniting all scientific concepts in a single conceptual system
-			on the basis of a few fundamental concepts.
-			<a href="https://en.wikipedia.org/wiki/Rudolf_Carnap#Der_Logische_Aufbau_der_Welt"
-				>[ src for this explanation ]</a
-			>
-			<a href="https://plato.stanford.edu/entries/carnap/aufbau.html"
-				>[ src for deeper explanation ]</a
-			>
-		</h6>
-	</span-->
 </main>
 
 <style>
@@ -115,6 +72,15 @@
 	@media only screen and (max-width: 768px) {
 		h6 {
 			font-size: 19px;
+		}
+
+		.top {
+			height: 100px;
+			margin-bottom: 30px;
+		}
+
+		.bottom {
+			height: calc(100vh - 150px);
 		}
 	}
 </style>
