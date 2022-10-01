56ecb2ca71048b0184f84455e42fa696f886da1d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-30 18:12:17 -0700

    edits

--- src/lib/components/pages/about.svelte
+++ src/lib/components/pages/about.svelte
@@ -2,35 +2,96 @@
 </script>
 
 <main>
-	<div class="text" />
+	<h2>AUFBAU</h2>
+	<h6 class="text">
+		Future-Focussed Web Studio Based in London. Aufbau is the creative outlet of Dan Humphries <a
+			href="https://dan.aufbau.io">[&nbsp;dan.aufbau.io&nbsp;]</a
+		>. Dan has 5+ years experience working across a range of tech and creative stacks, from data and
+		machine learning engineering, to graphic design and 3d development. He has a preference for
+		lightwight and performant code, and uses a combination of svelte.js, three.js, and supabase in
+		most projects. A child of the early internet, dan believes websites should be creative and fun,
+		not bland cut and paste jobs. He maintains independent research interests in neuroscience and
+		philosophy, in particular the notion of abstract and conceptual spaces, from which the name
+		aufbau is inhereted.
+	</h6>
+	<!--span class="muted">
+		<h2>THAT AUFBAU</h2>
+		<h6 class="text">
+			From 1922 to 1925, Rudolf Carnap worked on a book which became one of his major works, namely
+			Der logische Aufbau der Welt (translated as The Logical Structure of the World, 1967), which
+			was accepted in 1926 as his habilitation thesis at the University of Vienna and published as a
+			book in 1928. That achievement has become a landmark in modern epistemology and can be read as
+			a forceful statement of the philosophical thesis of logical positivism. Indeed, the Aufbau
+			suggests that epistemology, based on modern symbolic logic, is concerned with the logical
+			analysis of scientific propositions, while science itself, based on experience, is the only
+			source of knowledge of the external world, i.e. the world outside the realm of human
+			perception. According to Carnap, philosophical propositions are statements about the language
+			of science; they aren't true or false, but merely consist of definitions and conventions about
+			the use of certain concepts. In contrast, scientific propositions are factual statements about
+			the external reality. They are meaningful because they are based on the perceptions of the
+			senses. In other words, the truth or falsity of those propositions can be verified by testing
+			their content with further observations. In the Aufbau, Carnap wants to display the logical
+			and conceptual structure with which all scientific (factual) statements can be organized.
+			Carnap gives the label "constitution theory" to this epistemic-logical project. It is a
+			constructive undertaking that systematizes scientific knowledge according to the notions of
+			symbolic logic. Accordingly, the purpose of this constitutional system is to identify and
+			discern different classes of scientific concepts and to specify the logical relations that
+			link them. In the Aufbau, concepts are taken to denote objects, relations, properties, classes
+			and states. Carnap argues that all concepts must be ranked over a hierarchy. In that
+			hierarchy, all concepts are organized according to a fundamental arrangement where concepts
+			can be reduced and converted to other basic ones. Carnap explains that a concept can be
+			reduced to another when all sentences containing the first concept can be transformed into
+			sentences containing the other. In other words, every scientific sentence should be
+			translatable into another sentence such that the original terms have the same reference as the
+			translated terms. Most significantly, Carnap argues that the basis of this system is
+			psychological. Its content is the "immediately given", which is made of basic elements, namely
+			perceptual experiences. These basic elements consist of conscious psychological states of a
+			single human subject. In the end, Carnap argues that his constitutional project demonstrates
+			the possibility of defining and uniting all scientific concepts in a single conceptual system
+			on the basis of a few fundamental concepts.
+			<a href="https://en.wikipedia.org/wiki/Rudolf_Carnap#Der_Logische_Aufbau_der_Welt"
+				>[ src for this explanation ]</a
+			>
+			<a href="https://plato.stanford.edu/entries/carnap/aufbau.html"
+				>[ src for deeper explanation ]</a
+			>
+		</h6>
+	</span-->
 </main>
 
 <style>
 	main {
 		position: relative;
 		overflow: hidden;
+		height: 100vmin;
 		width: calc(100vw - 100px);
-		height: 100vh;
+		margin-left: 100px;
+		padding: 20px 20px 20px 20px;
+		overflow: auto;
+		background: var(--black);
+		color: var(--error);
 	}
 
 	.text {
 		top: 100px;
 		left: 5%;
 		user-select: none;
-		color: var(--white);
-		padding: 0 20px;
+		text-align: justify;
+		font-family: untitled-sans, sans-serif;
+		text-transform: uppercase;
 	}
 
-	.text h2 {
-		padding: 20px;
-		color: var(--white);
-		height: 100vh;
-		font-size: 200px;
-		line-height: calc(100vh / 4);
-		user-select: none;
-		font-family: nb-television, sans-serif;
-		overflow-wrap: break-word;
-		opacity: 0.9;
-		text-align: right;
+	h6 {
+		font-size: 50px;
+	}
+
+	a {
+		color: var(--error);
+	}
+
+	@media only screen and (max-width: 1000px) {
+		h6 {
+			font-size: 40px;
+		}
 	}
 </style>
--- src/lib/components/pages/contact.svelte
+++ src/lib/components/pages/contact.svelte
@@ -3,7 +3,9 @@
 
 <main>
 	<div class="text">
-		<h2>NO MORE DEAD WEB / EMAIL:<br /><a href="mailto: hi@aufbau.io">HI[at]<br />AUFBAU.IO</a></h2>
+		<h2>
+			MAKE WEB FUN AGAIN // MSG<br /><a href="mailto: hi@aufbau.io">HI[at]<br />AUFBAU.IO</a>
+		</h2>
 	</div>
 </main>
 
@@ -11,12 +13,11 @@
 	main {
 		position: relative;
 		overflow: hidden;
-		background: var(--black);
+		height: 100vh;
+		background: var(--error);
 	}
 
 	.text {
-		top: 100px;
-		left: 5%;
 		user-select: none;
 		padding: 0 20px;
 	}
@@ -24,12 +25,12 @@
 	.text h2 {
 		padding: 20px;
 		color: var(--white);
-		font-size: 200px;
+		font-size: calc((100vw - 100px) / 8);
 		line-height: calc(100vh / 4);
 		user-select: none;
 		font-family: nb-television, sans-serif;
 		overflow-wrap: break-word;
-		opacity: 0.9;
+
 		text-align: right;
 	}
 </style>
