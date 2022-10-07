0d8421906ccab8c10efe130ba458e4dfc2f54d44
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-10-07 01:40:06 -0700

    edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
 			<a href="/systems">modular architecture • example</a>
 		</div>
 		<div class="grey hidden body__left">
-			<p>bespoke websites, apps, systems</p>
+			<p>bespoke websites, systems, apps</p>
 			<p>ethos: make web fun again</p>
 		</div>
 		<div class="body__right">
