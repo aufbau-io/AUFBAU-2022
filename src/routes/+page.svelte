cfc1063fc691aa9ef67162ea57de0ee8fa7896ce
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-15 15:32:49 -0500

    mobile fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <script>
-	import { active } from '$lib/store/store';
-
-	let toggle = () => {
-		active.update((val) => !val);
-	};
 </script>
 
 <svelte:head>
@@ -11,54 +6,48 @@
 </svelte:head>
 
 <section>
-	{#if !$active}
-		<a href="/" class="centreButton alt" on:click={() => toggle()}
-			>make&nbsp;web&nbsp;fun&nbsp;again</a
-		>
-	{:else}
-		<div class="body">
-			<!--img class="niels" alt="niels" src="/niels_bw.jpg" /-->
+	<div class="body">
+		<!--img class="niels" alt="niels" src="/niels_bw.jpg" /-->
 
-			<div class="grey top">
-				<h1>aufbau.</h1>
+		<div class="grey top">
+			<h1>aufbau.</h1>
 
-				<p>CREATIVE WEB STUDIO</p>
-				<!-- <p on:click={() => toggle()}>make web fun again</p> -->
-				<p>ldn • ykt • nyc</p>
-			</div>
-			<div class="grey body__left">
-				<h3>work</h3>
-				<a href="https://iota.health" target="_blank">iota • app • WIP </a>
-
-				<a href="https://www.conceptioncalculator2000.com" target="_blank"
-					>conception calculator 2000 • web • WIP
-				</a>
-				<a href="https://replikamasterworks.com" target="_blank">replika • web • WIP </a>
-				<!-- <a href="https://site.hhacker.space" target="_blank">hedron hackerspace • web • 2022</a> -->
-				<!--a href="https://iota.health" target="_blank">iota • research app • 2022</a-->
-			</div>
-			<div class="grey body__right">
-				<h3>systems</h3>
-				<a href="/systems">modular system architecture • example</a>
-			</div>
-			<div class="grey hidden body__left">
-				<p>bespoke websites, systems, apps</p>
-				<p>lightweight and 3d specialist</p>
-			</div>
-			<div class="body__right">
-				<h3>say hi</h3>
-
-				<a target="_blank" href="/dan_humphries_cv.pdf">cv</a>
-				<a href="mailto: dan@aufbau.io">email</a>
-			</div>
-			<br />
+			<p>CREATIVE WEB STUDIO</p>
+			<!-- <p on:click={() => toggle()}>make web fun again</p> -->
+			<p>ldn • ykt • nyc</p>
 		</div>
-	{/if}
+		<div class="grey body__left">
+			<h3>work</h3>
+			<a href="https://iota.health" target="_blank">iota • app • WIP </a>
+
+			<a href="https://www.conceptioncalculator2000.com" target="_blank"
+				>conception calculator 2000 • web • WIP
+			</a>
+			<a href="https://replikamasterworks.com" target="_blank">replika • web • WIP </a>
+			<!-- <a href="https://site.hhacker.space" target="_blank">hedron hackerspace • web • 2022</a> -->
+		</div>
+		<div class="grey body__right">
+			<h3>systems</h3>
+			<a href="/systems">modular system architecture • example</a>
+		</div>
+		<div class="grey hidden body__left">
+			<p>bespoke websites, systems, apps</p>
+			<p>lightweight and 3d specialist</p>
+		</div>
+		<div class="body__right">
+			<h3>say hi</h3>
+
+			<a target="_blank" href="/dan_humphries_cv.pdf">cv</a>
+			<a href="mailto: dan@aufbau.io">email</a>
+		</div>
+		<br />
+	</div>
 </section>
 
 <style>
 	section {
-		height: 100%;
+		height: 100vh;
+		height: calc(var(--vh, 1vh) * 100);
 		display: flex;
 		overflow: auto;
 		max-width: 640px;
@@ -95,6 +84,8 @@
 		.body {
 			border-radius: 0;
 			padding: 16px 24px;
+			border-left: none;
+			border-right: none;
 		}
 	}
 </style>
