773df05019fd541593eeecbb56af9feb044fe65b
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-12-12 15:19:18 +0000

    edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,32 @@
 
 <main>
 	<section>
-		<h3>aufbau web studio</h3>
+		<h4>aufbau web studio</h4>
 	</section>
 
 	<body>
-		<a href="https://dan.aufbau.io" class="title">VISIT // PERSONAL WEBSITE</a>
-		<iframe src="https://dan.aufbau.io" title="PERSONAL" />
-		<a href="https://silicon.fm" class="title">VISIT // SILICON FM</a>
-		<iframe src="https://silicon.fm" title="SILICON" />
-		<a href="https://iota.health" class="title">VISIT // IOTA</a>
-		<iframe src="https://iota.health" title="IOTA" />
+		<div class="card">
+			<a href="https://dan.aufbau.io" class="title">VISIT // PERSONAL WEBSITE</a>
+			<iframe src="https://dan.aufbau.io" title="PERSONAL" />
+		</div>
+		<div class="card">
+			<a href="https://silicon.fm" class="title">VISIT // SILICON FM</a>
+			<iframe src="https://silicon.fm" title="SILICON" />
+		</div>
+		<div class="card">
+			<a href="https://iota.health" class="title">VISIT // IOTA</a>
+			<iframe src="https://iota.health" title="IOTA" />
+		</div>
+		<div class="card">
+			<a href="https://conceptioncalculator2000.com" class="title"
+				>VISIT CONCEPTION CALCULATOR 2000</a
+			>
+			<iframe src="https://conceptioncalculator2000.com" title="CC2000" />
+		</div>
 		<!-- <iframe src="https://replikamasterworks.com" title="IOTA" /> -->
 		<!-- <img src="system_diagram.png" alt="img" /> -->
 		<!-- <iframe src="https://siiignal.netlify.app" title="IOTA" /> -->
 		<!-- <iframe src="https://site.hhacker.space" title="IOTA" /> -->
-		<a href="https://conceptioncalculator2000.com" class="title">VISIT CONCEPTION CALCULATOR 2000</a
-		>
-		<iframe src="https://conceptioncalculator2000.com" title="CC2000" />
 		<!-- <iframe src="https://zen.aufbau.io" title="IOTA" /> -->
 		<!-- <iframe src="https://orange.aufbau.io" title="IOTA" /> -->
 	</body>
@@ -30,38 +39,32 @@
 <style>
 	iframe,
 	img {
-		width: 25%;
+		width: 100%;
 		height: calc(100% - 40px);
 	}
 
 	main {
 		height: 100vh;
-		width: 100vw;
 		height: calc(var(--vh, 1vh) * 100);
-
-		display: flex;
-		flex-flow: row wrap;
+		width: 100vw;
 	}
 
 	section {
-		position: sticky;
-		top: 0;
 		background: var(--background);
 		z-index: 10;
 
-		display: flex;
-		align-items: center;
-		justify-content: center;
 		padding: 0 16px;
 		overflow: auto;
 
 		touch-action: manipulation;
 	}
 
-	.title {
-		height: 40px;
+	.card {
 		width: 25%;
+		height: calc(100% - 20px);
+	}
 
+	.title {
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -88,28 +91,26 @@
 	}
 
 	body {
-		height: calc(100% - 40px);
+		height: 100%;
 
 		display: flex;
 		flex-flow: column wrap;
 		align-items: center;
-		border: 1px solid var(--primary-50);
+		overflow: hidden;
 	}
 
 	@media (max-width: 760px) {
 		body {
 			flex-flow: row wrap;
+			overflow: auto;
 		}
-		iframe,
-		img {
+		.card {
 			width: 100%;
 			height: 25%;
 		}
 
 		.title {
 			padding: 0;
-			width: 100%;
-			height: 20px;
 		}
 	}
 </style>
