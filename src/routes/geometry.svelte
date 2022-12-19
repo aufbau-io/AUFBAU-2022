2eb9908002274652c5aec79760d507810049597e
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-12-19 23:11:52 +0000

    update

--- src/routes/geometry.svelte
+++ src/routes/geometry.svelte
@@ -108,7 +108,7 @@
 	element3.style.width = `800px`;
 	element3.style.height = `800px`;
 	element3.style.opacity = 1;
-	element3.src = 'https://replikamasterworks.com';
+	element3.src = 'https://dan.aufbau.io';
 
 	var domObject3 = new CSS3DObject(element3);
 	domObject3.position.x = 400;
@@ -158,7 +158,7 @@
 
 	var material = new THREE.MeshToonMaterial({
 		opacity: 0,
-		color: new THREE.Color('0xd0d0d0'),
+		color: new THREE.Color('0x232323'),
 		side: THREE.DoubleSide
 	});
 	var geometry_plane = new THREE.BoxGeometry(799, 799, 799);
@@ -202,67 +202,12 @@
 	controls.enablePan = false;
 	controls2.enablePan = false;
 
-	// Setting up a group to hold the items we will be creating together
-	let group = new THREE.Group();
-
-	// Array curve holds the positions of points generated from a lorenz equation; lorenz function below generates the points and returns an array of points
-	let arrayCurve = lorenz();
-
-	// Generating the geometry
-	let curve = new THREE.CatmullRomCurve3(arrayCurve);
-	let vertices = curve.getPoints(100000);
-	let geometry = new THREE.BufferGeometry().setFromPoints(vertices);
-
-	// Generating a cloud of point
-	let pcMat = new THREE.PointsMaterial();
-	pcMat.color = new THREE.Color(0x232323);
-	pcMat.transparent = false;
-	pcMat.size = 1;
-	// pcMat.blending = THREE.AdditiveBlending;
-	pc = new THREE.Points(geometry, pcMat);
-	pc.sizeAttenuation = true;
-	pc.sortPoints = true;
-
-	group.add(pc);
-	group.scale.set(50, 50, 50);
-
-	// scene.add(group);
-
 	scene2.rotation.y += Math.PI;
-
-	let step = 0;
 
 	let render = function () {
 		renderer.render(scene, camera);
 		renderer2.render(scene2, camera);
 		id = requestAnimationFrame(render);
-
-		// //Varying the points on each frame
-		// step += 0.00001;
-		// // console.log(step);
-		// let geometry = pc.geometry;
-		// let a = 0.9 + Math.random() * 7;
-		// let b = 3.4 + Math.random() * 8;
-		// let f = 9.9 + Math.random() * 9;
-		// let g = 1 + Math.random();
-		// let t = 0.0001;
-
-		// // geometry.vertices.forEach(function (v) {
-		// // 	v.x = v.x - t * a * v.x + t * v.y * v.y - t * v.z * v.z + t * a * f;
-		// // 	v.y = v.y - t * v.y + t * v.x * v.y - t * b * v.x * v.z + t * g;
-		// // 	v.z = v.z - t * v.z + t * b * v.x * v.y + t * v.x * v.z;
-		// // });
-		// // geometry.verticesNeedUpdate = true;
-
-		// const positions = geometry.attributes.position.array;
-		// for (let i = 0; i < positions.length; i += 3) {
-		// 	let v = new THREE.Vector3(positions[i], positions[i + 1], positions[i + 2]);
-		// 	positions[i] = v.x - t * a * v.x + t * v.y * v.y - t * v.z * v.z + t * a * f;
-		// 	positions[i + 1] = v.y - t * v.y + t * v.x * v.y - t * b * v.x * v.z + t * g;
-		// 	positions[i + 2] = v.z - t * v.z + t * b * v.x * v.y + t * v.x * v.z;
-		// }
-
-		// geometry.attributes.position.needsUpdate = true;
 
 		// group.rotation.x += 0.002;
 		// group.rotation.y += 0.002;
@@ -283,26 +228,6 @@
 	// );
 
 	render();
-
-	function lorenz() {
-		let arrayCurve = [];
-
-		let x = 0.01,
-			y = 0.01,
-			z = 0.01;
-		let a = 4.9;
-		let b = 5.4;
-		let f = 7.9;
-		let g = 1;
-		let t = 0.0006;
-		for (let i = 0; i < 100000; i++) {
-			x = x - t * a * x + t * y * y - t * z * z + t * a * f;
-			y = y - t * y + t * x * y - t * b * x * z + t * g;
-			z = z - t * z + t * b * x * y + t * x * z;
-			arrayCurve.push(new THREE.Vector3(x, y, z).multiplyScalar(2));
-		}
-		return arrayCurve;
-	}
 </script>
 
 <div bind:this={container} class:geometry={true} />
