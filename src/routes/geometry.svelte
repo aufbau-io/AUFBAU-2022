c290b4a1fb3d06cd1463d10dfac7f23004fc5461
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-01-19 21:53:05 +0000

    edit

--- src/routes/geometry.svelte
+++ src/routes/geometry.svelte
@@ -193,9 +193,9 @@
 	const positions = new Float32Array(particlesCount * 3);
 
 	for (let i = 0; i < particlesCount; i++) {
-		positions[i * 3 + 0] = (Math.random() - 0.5) * 3000;
-		positions[i * 3 + 1] = (Math.random() - 0.5) * 3000;
-		positions[i * 3 + 2] = (Math.random() - 0.5) * 3000;
+		positions[i * 3 + 0] = (Math.random() - 0.5) * 3200;
+		positions[i * 3 + 1] = (Math.random() - 0.5) * 3200;
+		positions[i * 3 + 2] = (Math.random() - 0.5) * 3200;
 	}
 
 	const particlesGeometry = new THREE.BufferGeometry();
