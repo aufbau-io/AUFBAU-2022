6059e298b9643e6cd75b99882d9914a9d41ba527
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-03-19 23:30:10 +0000

    unedit

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
-export const active = writable(true);
+export const active = writable(false);
 export const screenType = writable(1);
 export const demoSrc = writable(null);
