b74fd902b7099b15f7e7e087a9749b0da56168eb
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-03-19 22:44:22 +0000

    black background on mobile

--- src/routes/(pages)/systems/+page.svelte
+++ src/routes/(pages)/systems/+page.svelte
@@ -13,11 +13,15 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		background: var(--black-50);
 	}
 
 	.img {
 		max-height: 100vh;
 		max-width: 100vw;
+	}
+	@media (max-width: 800px) {
+		main {
+			background: var(--black);
+		}
 	}
 </style>
